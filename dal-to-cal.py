8a9817480b35d4ba346cada17f505f284b242cdc
Alex Yorke <alhexyorke@users.noreply.github.com>
2018-09-03 23:13:12 -0300

    Prompts user to login; improve user interface

--- dal-to-cal.py
+++ dal-to-cal.py
@@ -1,3 +1,35 @@
+import shutil, sys
+import random
+import string
+
+# log the user in and get the schedule page
+import requests
+import getpass
+
+print("=========================================================")
+print("Dal-to-cal will create a schedule for the Fall 2018 term.")
+print("=========================================================")
+
+username = input("Enter your Dal NetID (not your CSID!):")
+password = getpass.getpass("Enter your Dal NetID password (won't show up when you're typing):")
+
+# go to login page to get initial cookies (weird but ok)
+s = requests.Session()
+res = s.get("https://dalonline.dal.ca/PROD/twbkwbis.P_ValLogin")
+cookies = dict(res.cookies)
+
+# try to login
+response = s.post('https://dalonline.dal.ca/PROD/twbkwbis.P_ValLogin', cookies=cookies, allow_redirects=True, headers=None, data=("sid=" + username + "&PIN=" + password))
+
+# check credentials
+if "Incorrect NetID or password. Please try again." in response.text:
+    print("Incorrect password or username! Try again.")
+    exit()
+	
+schedule = s.post('https://dalonline.dal.ca/PROD/bwskfshd.P_CrseSchdDetl', cookies=cookies, data="term_in=201910").text
+
+print("I'm working my magic. Please wait...")
+
 try:
     import urllib.request as urllib2
 except ImportError:
@@ -13,11 +45,7 @@ import pytz
 
 timezone = pytz.timezone('America/Halifax')
 
-scheduleHTML = ""
-
-with open("schedule_view_source.txt", "rb") as scheduleFile:
-    for line in scheduleFile:
-        scheduleHTML = scheduleHTML + (line.decode(errors='ignore'))
+scheduleHTML = schedule
 
 # scrape the schedule from the HTML page
 soup = BeautifulSoup(scheduleHTML, "html.parser")
@@ -141,3 +169,17 @@ f = open("dal_schedule.ics", "w")
 contents = "".join(contents)
 f.write(contents)
 f.close()
+
+print("Finished creating your schedule! You must be excited.")
+
+response = input("Would you like to download your schedule using a web browser? (recommended): type y or n: ")
+
+if (response == "y"):
+    random_filename = "dal_schedule_" + ''.join(random.choices(string.ascii_uppercase + string.digits, k=14)) + ".ics"
+    calendar_path = "/users/webhome/" + getpass.getuser() + "/" + random_filename
+    shutil.move("dal_schedule.ics", calendar_path)
+    print("Remember to delete the file from " + calendar_path + " when you are done!")
+    print("")
+    print("Pssst! It's available at https://web.cs.dal.ca/~/" + getpass.getuser() + "/" + random_filename)
+else:
+    print("No problem. I created the file called dal_schedule.ics which you can download later.")
